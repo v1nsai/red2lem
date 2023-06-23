90cf92b6d4d0d2e5e0c54515663135d7f6380e36
Ross Wardrup <minorsecond@gmail.com>
2023-06-23 09:50:30 -0600

    Add author name to post, in square brackets
    Also added an option to prevent automod posts from being copied, as many times these would be unwanted.

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ from . import credentials
 from . import vars
 from time import sleep
 
+copy_automoderator_posts = False
+
 # Create a Reddit instance
 def get_reddit():
     return praw.Reddit(
@@ -15,10 +17,12 @@ def get_reddit():
         user_agent=vars.user_agent,
     )
 
+
 def get_lemmy():
     lemmy = Lemmy(credentials.lemmy_url)
     lemmy.log_in(credentials.lemmy_username, credentials.lemmy_password)
     return lemmy
+
 
 if __name__ == '__main__':
 
@@ -31,39 +35,42 @@ if __name__ == '__main__':
     # Get the lemmy object
     lemmy = get_lemmy()
     community_id = lemmy.discover_community(vars.community_name)
-    
+
     # Watch subreddit for new posts
     for submission in subreddit.stream.submissions():
         print(f"New post found: {submission.title} ({submission.url})")
 
+        author_name = submission.author.name
+        # Don't copy automoderator posts
+        if author_name == "AutoModerator" and not copy_automoderator_posts:
+            print("Not copying automoderator content")
+            continue
+
+        title = f"[reddit user {author_name}] - {submission.title}"
         if submission.is_video:
             print("Saving video...")
-            title = submission.title
             media_url = submission.media['reddit_video']['fallback_url']
             media_type = media_url.split('.')[-1]
             media_type = re.sub(r'\?.*', '', media_type)
             media_content = requests.get(media_url).content
         elif submission.url.endswith(('.jpg', '.jpeg', '.png', '.gif')):
             print("Saving image...")
-            title = submission.title            
             media_url = submission.url
             media_type = submission.url.split('.')[-1]
             media_content = requests.get(media_url).content
         elif submission.is_self:
             print("Saving text...")
-            title = submission.title
             media_type = 'txt'
             media_content = submission.selftext
             media_url = ''
-            lemmy.post.create( community_id=community_id, name=title, body=media_content )
+            lemmy.post.create(community_id=community_id, name=title, body=media_content)
             continue
         else:
             print("Saving link...")
-            title = submission.title
             media_type = 'link'
             media_url = submission.url
             media_content = ''
-        
+
         print("Uploading to lemmy...")
-        lemmy.post.create( community_id=community_id, name=title, url=media_url, body=media_content )
-        sleep(10) # flooding is bad and likely to get you banned
+        lemmy.post.create(community_id=community_id, name=title, url=media_url, body=media_content)
+        sleep(10)  # flooding is bad and likely to get you banned
