b7093d7d9ee0c211b467b3bc5a74658f4bf6e91d
vp-en <67713433+vp-en@users.noreply.github.com>
2023-08-10 17:33:08 +0200

    Revamp
    Reworking most of the project, it works pretty good now

--- bot.py
+++ bot.py
@@ -1,76 +1,119 @@
 import praw
 import requests
-import re
-
+import os
+import time
 from pythorhead import Lemmy
-from . import credentials
-from . import vars
-from time import sleep
+from imgurpython import ImgurClient
 
-copy_automoderator_posts = False
+# Detect Docker
+if os.environ.get("container", False):
+    print("Running as a container...")
 
-# Create a Reddit instance
+
+# Function to get Reddit API client
 def get_reddit():
     return praw.Reddit(
-        client_id=credentials.reddit_client_id,
-        client_secret=credentials.reddit_client_secret,
-        user_agent=vars.user_agent,
+        client_id=os.environ["reddit_client_id"],
+        client_secret=os.environ["reddit_client_secret"],
+        user_agent=os.environ["reddit_user_agent"],
     )
 
 
+# Function to get Lemmy API client
 def get_lemmy():
-    lemmy = Lemmy(credentials.lemmy_url)
-    lemmy.log_in(credentials.lemmy_username, credentials.lemmy_password)
+    lemmy = Lemmy(os.environ["lemmy_url"])
+    lemmy.log_in(os.environ["lemmy_username"], os.environ["lemmy_password"])
     return lemmy
 
 
-if __name__ == '__main__':
+# Function to upload media to Imgur
+def upload_to_imgur(media_url):
+    imgur_client_id = os.environ.get("imgur_client_id")
+    imgur_client_secret = os.environ.get("imgur_client_secret")
+    client = ImgurClient(client_id=imgur_client_id, client_secret=imgur_client_secret)
+    return client.upload_from_url(media_url, config=None, anon=True)["link"]
 
-    # TODO if api fails use json
 
+# Function to get a Reddit submission object from a submission ID
+def get_submission_from_json(submission_id):
+    url = f"https://www.reddit.com/comments/{submission_id}.json"
+    response = requests.get(
+        url, headers={"User-agent": os.environ["reddit_user_agent"]}
+    )
+    json_data = response.json()
+    submission_data = json_data[0]["data"]["children"][0]["data"]
+    return praw.models.Submission(reddit=None, _data=submission_data)
+
+
+# Main function to watch Reddit for new posts and post them to Lemmy
+def main():
     # Get the subreddit object
     reddit = get_reddit()
-    subreddit = reddit.subreddit(subreddit_name)
+    subreddit = reddit.subreddit(os.environ["subreddit_name"])
 
     # Get the lemmy object
     lemmy = get_lemmy()
-    community_id = lemmy.discover_community(vars.community_name)
+    community_id = lemmy.discover_community(os.environ["community_name"])
+
+    # Sleep time between checking new posts, default is 60 seconds
+    sleep_time = int(os.environ.get("post_check_interval", 60))
 
     # Watch subreddit for new posts
     for submission in subreddit.stream.submissions():
+        time.sleep(sleep_time)
         print(f"New post found: {submission.title} ({submission.url})")
 
-        author_name = submission.author.name
+        try:
+            author_name = submission.author.name
+        except Exception:
+            print(
+                f"Failed to get submission from API for {submission.id}, falling back to JSON..."
+            )
+            submission = get_submission_from_json(submission.id)
+            author_name = submission.author.name
+
         # Don't copy automoderator posts
+        copy_automoderator_posts = os.environ.get("copy_automoderator_posts", False)
         if author_name == "AutoModerator" and not copy_automoderator_posts:
             print("Not copying automoderator content")
             continue
 
-        title = f"[reddit user {author_name}] - {submission.title}"
-        if submission.is_video:
-            print("Saving video...")
-            media_url = submission.media['reddit_video']['fallback_url']
-            media_type = media_url.split('.')[-1]
-            media_type = re.sub(r'\?.*', '', media_type)
-            media_content = requests.get(media_url).content
-        elif submission.url.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            print("Saving image...")
-            media_url = submission.url
-            media_type = submission.url.split('.')[-1]
-            media_content = requests.get(media_url).content
-        elif submission.is_self:
-            print("Saving text...")
-            media_type = 'txt'
-            media_content = submission.selftext
-            media_url = ''
-            lemmy.post.create(community_id=community_id, name=title, body=media_content)
-            continue
+        # Check if media exists and if it contains 'reddit_video'
+        if submission.media is not None and "reddit_video" in submission.media:
+            media_url = submission.media["reddit_video"]["fallback_url"]
+            imgur_client_id = os.environ.get("imgur_client_id")
+            if imgur_client_id:
+                media_url = upload_to_imgur(media_url)
+                print("Video found, uploading to imgur...")
         else:
-            print("Saving link...")
-            media_type = 'link'
             media_url = submission.url
-            media_content = ''
+
+        # Construct the link to the original Reddit post
+        original_post_link = (
+            f"**Original Post**: [link to OP](https://np.reddit.com{submission.permalink})"
+        )
+
+        # Create title and body for Lemmy post
+        title = f"[X-Posted from Reddit - /u/{author_name}] - {submission.title}"
+        body = f"{submission.selftext}\n\n{original_post_link}"
 
         print("Uploading to lemmy...")
-        lemmy.post.create(community_id=community_id, name=title, url=media_url, body=media_content)
-        sleep(10)  # flooding is bad and likely to get you banned
+        lemmy_post = lemmy.post.create(
+            community_id=community_id, name=title, url=media_url, body=body
+        )
+
+        # Check if the Lemmy post was created successfully
+        if lemmy_post:
+            print("Lemmy post created successfully")
+            # Get and print the actual URL of the created post
+            lemmy_post_url = lemmy_post["post_view"]["post"]["ap_id"]
+            print(f"Post URL: {lemmy_post_url}")
+        else:
+            print("Failed to create Lemmy post")
+
+        # Sleep for the configured interval before checking for new posts
+        time.sleep(sleep_time)
+
+
+if __name__ == "__main__":
+    main()
